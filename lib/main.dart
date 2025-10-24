647078d28c49b2cf0b9acfe5d48723131b90e61f
Joel Funkhouser <tech@enspiremedia.com>
2025-10-24 08:58:37 -0700

    Initialize Flutter app with MapScreen

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'map_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mapbox Polygon POC',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const MapScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
